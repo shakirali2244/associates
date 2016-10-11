69199577f48977a81939f82b67ff3201177ecc52
Shakir Ali <Shakir Ali>
2016-10-11 07:16:14 -0700

    2150 assn2

---
+++ CPSC_2150/2_dyn_array/Array.cpp
@@ -0,0 +1,200 @@
+#include "Array.h"
+
+#include <iostream>
+//#include <limits>
+//#include <ios>
+using namespace std;
+Array::Array(){
+	list = nullptr;
+}
+
+bool Array::isEmpty(){
+	return (list == nullptr);
+}
+
+int Array::length(){
+	int ret = 0;
+	Node* head = list;
+	while(head != nullptr){
+		ret++;
+		head = head ->next;
+	}
+	return ret;
+}
+
+Array::Node* Array::cons(char x, Node* p){
+   return new Node{x, p};
+}
+
+bool Array::append(char a){
+	return append(a,list);
+}
+
+bool Array::append(char a, Node* head){
+	if (length() == 0){
+		list = cons(a, nullptr);
+		return true;
+	}
+	if(head-> next == nullptr){
+		head->next = cons(a,nullptr);
+		return true;
+	}
+	else{
+		return true && append(a,head->next);
+	}
+}
+
+bool Array::remove(char a){
+	if (list != nullptr && list->next != nullptr){
+		if (list->item != a)
+			return remove(a, list->next, list);
+		else{
+			Node* tmp = list;
+			list = list->next;
+			delete tmp;
+			tmp = nullptr;
+			return true;
+		}
+			
+	}else{
+		if(list != nullptr && list->item == a){
+			delete list;
+			list = nullptr;
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
+
+bool Array::remove(char a, Node* head, Node* previous){
+	if( head == nullptr ){
+		return false;
+	}
+	//std::cout << "... "<< head->info << " " << (head -> info == a) << std::endl;
+	if( head -> item == a){
+		Node* tmp = head->next;
+		previous -> next = tmp;
+		delete head;
+		head = nullptr;
+		return true;
+	}else {
+		return remove(a, head->next, head);
+	}
+}
+
+bool Array::search(char a){
+	return search(a, list);
+}
+
+bool Array::search(char a, Node* head){
+	if(head == nullptr){
+		return false;
+	}else if(head->item == a){
+		return true;
+	}else{
+		return search(a,head->next);
+	}
+}
+
+char Array::getFirst(){
+	if(list != nullptr) return list->item;
+	else return '\0';
+}
+	
+
+void Array::printList(std::ostream& fout) {
+   printList(fout, list);
+}
+
+void Array::reverse(){
+	if (length() <= 1) return;
+	if (length() == 2){
+		Node* tmp = list;
+		list = list->next;
+		list->next = tmp;
+		return;
+	}
+	
+	Node* previous = list;
+	Node* head = previous -> next;
+	Node* front= head->next;
+	
+	previous->next = nullptr;
+	while(head->next != nullptr){
+		head->next = previous;
+		Node* tmp = front->next;
+		front->next = head; 
+		previous = head;
+		head = front;
+		front = tmp;
+	}
+	list = head;
+	return;
+}
+		
+		
+		
+		
+	
+void Array::printList(std::ostream& fout, Node* p) {
+   fout << "[ ";
+   while (p != nullptr){
+      fout << p -> item;
+      if (p->next != nullptr)
+	      fout << ", ";
+      p = p -> next;
+   }
+   fout << " ]\n";
+}
+	
+		
+
+/*
+
+CList::Node* CList::cons(int x, Node* p){
+   return new Node{x, p};
+}
+
+void CList::readList(std::istream& fin){
+   //deleteList(list);
+   list = readBackwardList(fin);
+}
+
+CList::Node* CList::readBackwardList(std::istream& fin) {
+   Node* p = nullptr;
+   Node *tail = p;
+   int x;
+   while(fin >> x){
+      p = cons(x, p);
+      if (tail == nullptr){
+         tail = p;
+      }
+   }
+   tail->next = p;
+   //reset the stream out of fail state
+   fin.clear();
+   //consume characters encountered to the end of the line
+   fin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+   return p;
+}
+
+void CList::printList(std::ostream& fout) {
+   printList(fout, list);
+}
+void CList::printList(std::ostream& fout, Node* p) {
+   fout << "[ ";
+   p = p->next;
+   while (p != list){
+      fout << p -> info << " ";
+      p = p -> next;
+   }
+   fout << " ]\n";
+}
+
+
+void CList::deleteList(Node* &p){
+   std::cerr << "\n  HUMONGOUS MEMORY LEAK -- worth failing the lab \n";
+   p = nullptr;
+}
+*/
---
+++ CPSC_2150/2_dyn_array/testArray.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "Array.h"
+
+using namespace std;
+int main(){
+	Array l1; 
+	Array l2;
+	cout << l2.getFirst() << endl;
+	l2.append('a');
+	l2.append('b');
+	l2.append('a');
+	l2.append('4');
+	l2.append('a');
+	l2.printList(cout);
+	//l2.reverse();
+	l2.printList(cout);
+	l2.remove('a');
+	cout << "l2.search('a')" << l2.search('a') << endl;
+	l2.printList(cout);
+	l2.remove('a');
+	cout << "l2.search('a')" << l2.search('a') << endl;
+	l2.printList(cout);
+	cout << "l2.remove('a')" << l2.remove('a') << endl;
+	cout << "l2.remove('a')" << l2.remove('a') << endl;
+	cout << "l2.search('a')" << l2.search('a') << endl;
+	//cout << "l1.isEmpty() = " << l1.isEmpty() <<" l1.length() = " <<  l1.length() << endl;
+	cout << "l2.isEmpty() = " << l2.isEmpty() <<" l2.length() = " <<  l2.length() << endl;	
+	return 0;
+}
